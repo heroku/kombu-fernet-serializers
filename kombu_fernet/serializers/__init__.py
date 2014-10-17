55ab8456e9dafc6b724c5987e433ec0fb46fcf6c
David Gouldin <dgouldin@heroku.com>
2014-10-16 17:27:22 -0700

    Ripping out fernet-py remnants, bumping version

--- kombu_fernet/serializers/__init__.py
+++ kombu_fernet/serializers/__init__.py
@@ -2,52 +2,19 @@
 from __future__ import unicode_literals, absolute_import
 
 import os
-key = os.environ['KOMBU_FERNET_KEY']
-ttl = os.environ.get('KOMBU_FERNET_TTL', 60)
-enforce_ttl = os.environ.get('KOMBU_FERNET_ENFORCE_TTL', '').lower() == 'true'
+from cryptography.fernet import Fernet, InvalidToken
 
-class InvalidToken(Exception):
-    pass
+fernet = Fernet(os.environ['KOMBU_FERNET_KEY'])
 
-try:
-    from cryptography.fernet import Fernet, InvalidToken as CInvalidToken
-except ImportError:
-    try:
-        import fernet
-    except ImportError:
-        raise RuntimeError("Either cryptography or fernet-py is required")
+def fernet_encode(func):
+    def inner(message):
+        return fernet.encrypt(func(message))
+    return inner
 
-    fernet.Configuration.enforce_ttl = enforce_ttl
-    fernet.Configuration.ttl = ttl
-
-    def fernet_encode(func):
-        def inner(message):
-            return fernet.generate(key, func(message))
-        return inner
-
-    def fernet_decode(func):
-        def inner(encoded_message):
-            verifier = fernet.verifier(key, encoded_message)
-            if verifier.valid():
-                return func(verifier.message)
-            else:
-                raise InvalidToken
-        return inner
-else:
-    fernet = Fernet(key)
-
-    def fernet_encode(func):
-        def inner(message):
-            return fernet.encrypt(func(message))
-        return inner
-
-    def fernet_decode(func):
-        def inner(encoded_message):
-            if isinstance(encoded_message, unicode):
-                encoded_message = encoded_message.encode('utf-8')
-            try:
-                message = fernet.decrypt(encoded_message)
-            except CInvalidToken:
-                raise InvalidToken
-            return func(message)
-        return inner
+def fernet_decode(func):
+    def inner(encoded_message):
+        if isinstance(encoded_message, unicode):
+            encoded_message = encoded_message.encode('utf-8')
+        message = fernet.decrypt(encoded_message)
+        return func(message)
+    return inner

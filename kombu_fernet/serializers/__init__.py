3956293089e500ef1f7d665bc3fcf45706fc5d6b
Chris Lambacher <clambacher@heroku.com>
2016-02-10 12:17:59 -0500

    Add ability to in-place rotate fernet key.

--- kombu_fernet/serializers/__init__.py
+++ kombu_fernet/serializers/__init__.py
@@ -5,16 +5,26 @@ import os
 from cryptography.fernet import Fernet, InvalidToken
 
 fernet = Fernet(os.environ['KOMBU_FERNET_KEY'])
+fallback_fernet = None
+try:
+    fallback_fernet = Fernet(os.environ['OLD_KOMBU_FERNET_KEY'])
+except KeyError:
+    pass
+
 
 def fernet_encode(func):
     def inner(message):
         return fernet.encrypt(func(message))
     return inner
 
+
 def fernet_decode(func):
     def inner(encoded_message):
         if isinstance(encoded_message, unicode):
             encoded_message = encoded_message.encode('utf-8')
-        message = fernet.decrypt(encoded_message)
+        try:
+            message = fernet.decrypt(encoded_message)
+        except InvalidToken:
+            message = fallback_fernet.decrypt(encoded_message)
         return func(message)
     return inner

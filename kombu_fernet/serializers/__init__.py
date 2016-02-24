37ed95bf8a56642683ce177e7b6142c54776aca4
Di Wu <dw323@cornell.edu>
2016-02-23 19:36:15 -0800

    make fernet lazy instantiate so that env variables can be dyanmically set in test

--- kombu_fernet/serializers/__init__.py
+++ kombu_fernet/serializers/__init__.py
@@ -4,19 +4,24 @@ from __future__ import unicode_literals, absolute_import
 import os
 from cryptography.fernet import Fernet, MultiFernet
 
-fernet = Fernet(os.environ['KOMBU_FERNET_KEY'])
-fallback_fernet = None
-try:
-    fallback_fernet = Fernet(os.environ['KOMBU_FERNET_KEY_PREVIOUS'])
-except KeyError:
-    pass
-else:
-    fernet = MultiFernet([fernet, fallback_fernet])
+
+def lazy_fernet():
+    if not hasattr(lazy_fernet, 'fernet'):
+        fernet = Fernet(os.environ['KOMBU_FERNET_KEY'])
+        fallback_fernet = None
+        try:
+            fallback_fernet = Fernet(os.environ['KOMBU_FERNET_KEY_PREVIOUS'])
+        except KeyError:
+            pass
+        else:
+            fernet = MultiFernet([fernet, fallback_fernet])
+        lazy_fernet.fernet = fernet
+    return lazy_fernet.fernet
 
 
 def fernet_encode(func):
     def inner(message):
-        return fernet.encrypt(func(message))
+        return lazy_fernet().encrypt(func(message))
     return inner
 
 
@@ -24,6 +29,6 @@ def fernet_decode(func):
     def inner(encoded_message):
         if isinstance(encoded_message, unicode):
             encoded_message = encoded_message.encode('utf-8')
-        message = fernet.decrypt(encoded_message)
+        message = lazy_fernet().decrypt(encoded_message)
         return func(message)
     return inner
